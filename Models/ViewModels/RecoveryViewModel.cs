f7fc3522d7d41bfde34b421ca33aa8a38267f6c4
crism03 <cristianmeza440@gmail.com>
2026-04-20 00:30:58 -0300

    carpeta RecoveryViewModel

--- Models/ViewModels/RecoveryViewModel.cs
+++ Models/ViewModels/RecoveryViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace axiomtechno.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace axiomtechno.Models.ViewModels
 {
     public class RecoveryViewModel
     {
+        [EmailAddress]
+        [Required(ErrorMessage = "El campo email es obligatorio.")]
+        public string? UsEmail { get; set; }
     }
 }

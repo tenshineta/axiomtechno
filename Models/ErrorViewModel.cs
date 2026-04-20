10fd10e86493207227a09f640d463bcf8b758911
Ángel <ansansoni23@gmail.com>
2026-04-19 23:16:34 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace axiomtechno.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

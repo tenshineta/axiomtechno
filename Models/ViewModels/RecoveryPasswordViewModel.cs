96dbaf8ba327ae8c4002c69fecb7bee57f394f46
florenrom <floren6519@gmail.com>
2026-04-20 00:07:39 -0300

    añadido de RecoveryPasswordViewModel

--- Models/ViewModels/RecoveryPasswordViewModel.cs
+++ Models/ViewModels/RecoveryPasswordViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace axiomtechno.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace axiomtechno.Models.ViewModels
 {
     public class RecoveryPasswordViewModel
     {
+        public string? token { get; set; }
+
+        [Required]
+        [Display(Name = "DNI")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "El DNI debe tener 8 dígitos.")]
+        public string? UsDNI { get; set; }
+
+        [Required]
+        [Display(Name = "Nueva contraseña")]
+        public string? UsPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("UsPassword", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string? UsPasswordConfirm { get; set; }
     }
 }

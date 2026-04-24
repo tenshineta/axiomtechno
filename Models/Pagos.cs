bafbb6551a34af5d6df6bf35a70aaea7f7a375a3
crism03 <cristianmeza440@gmail.com>
2026-04-24 01:14:33 -0300

    validaciones de Pagos

--- Models/Pagos.cs
+++ Models/Pagos.cs
@@ -7,10 +7,19 @@ namespace axiomtechno.Models
     {
         [Key]
         public int PagId { get; set; }
+        [Display(Name = "Monto del pago")]
+        [Required(ErrorMessage = "Debe ingresar un monto.")]
+        [Range(0.1, 9999999999.99, ErrorMessage = "El monto debe ser mayor a 0.")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal PagMonto { get; set; }
+        [Display(Name = "Fecha del pago")]
+        [Required(ErrorMessage = "Debe ingresar una fecha del pago.")]
+        [DataType(DataType.Date)]
         public DateOnly PagFecha { get; set; }
+        [Display(Name = "Cliente")]
+        [Required(ErrorMessage = "Debe seleccionar un cliente.")]
         public int ClId { get; set; }
+        [ForeignKey("ClId")]
         public Clientes? Cliente { get; set; }
     }
 }

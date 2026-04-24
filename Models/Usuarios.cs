9bbdf4670687d630c6d7b54fec6fb8a34b1823f3
Ángel <ansansoni23@gmail.com>
2026-04-24 00:58:22 -0300

    Eliminación de archivo extra y creación correcta de validaciones de usuarios

--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -8,19 +8,49 @@ namespace axiomtechno.Models
     {
         [Key]
         public int UsId { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[\p{L}\s'-]+$", ErrorMessage = "El nombre solo debe contener letras y espacios.")]
+        [Display(Name = "Nombre de usuario")]
         public string UsNombre { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[\p{L}\s'-]+$", ErrorMessage = "El apellido solo debe contener letras y espacios.")]
+        [Display(Name = "Apellido de usuario")]
         public string UsApellido { get; set; }
+
+        [Required]
+        [Range(6000000, 999999999, ErrorMessage = "El DNI debe tener 8 números exactos.")]
+        [Display(Name = "DNI de usuario")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "El DNI debe tener 8 números exactos.")]
         public long? UsDni { get; set; }
+
         public DateTime UsFechaNacimiento { get; set; }
+
         public DateTime UsFechaCreacion { get; set; } = DateTime.Now;
+
+        [Required]
+        [Display(Name = "Correo de usuario")]
+        [EmailAddress(ErrorMessage = "El formato del correo es inválido.")]
         public string UsCorreo { get; set; }
+
+        [Required]
+        [MinLength(8)]
         public string UsPasswordHash { get; set; }
+
         public string? token_recovery { get; set; }
+
+        [Display(Name = "¿Usuario activo?")]
         public bool UsActivo { get; set; }
+
+        [Display(Name = "Número de celular")]
         public long UsTelefono { get; set; }
+
         [NotMapped]
         public string NombreCompleto => $"{UsNombre} {UsApellido}";
+
         public int RolID { get; set; }
+
         public virtual Roles? Rol { get; set; }
     }
 }
